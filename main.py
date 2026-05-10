d5c12988b6ea8fd4fd5cb64191c1ee13d6fe1994
wxhqaq <2911841277@qq.com>
2026-05-11 01:23:36 +0800

    完成该阶段所有目标

--- main.py
+++ main.py
@@ -10,23 +10,49 @@ from validator import to_number, is_valid_operator
 
 
 def show_welcome():
-    print("欢迎使用简易计算器\n")
+    print("欢迎使用简易计算器")
 
 
-def get_number():
-    pass
+def get_number(prompt):
+    while True:
+        value = input(prompt).strip()
+        try:
+            return to_number(value)
+        except ValueError as e:
+            print(e)
 
 
 def get_operator():
-    pass
+    while True:
+        value = input("op:").strip()
+        if is_valid_operator(value):
+            return value
+        else:
+            print("操作符非法")
 
 
 def ask_continue():
-    pass
+    while 1:
+        choice = input("是否继续计算？[y/n]:").strip().lower()
+        if choice == 'y':
+            return True
+        else:
+            return False
 
 
 def run_calculator():
-    pass
+    show_welcome()
+    while 1:
+        a = get_number("a:")
+        op = get_operator()
+        b = get_number("b:")
+
+        try:
+            print("output:", calculate(a, b, op))
+        except ValueError as e:
+            print(e)
+        if ask_continue() == False:
+            break
 
 
 if __name__ == "__main__":
